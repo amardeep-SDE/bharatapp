438a956119c6984d7778d07187737ca6490044c7
Amardeep Dwivedi <amardeepdwivedi1494@gmail.com>
2026-06-20 23:00:05 +0530

    fix: app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,11 @@
 import express from "express";
+// @ts-ignore: No types for 'cors' installed
 import cors from "cors";
 import authRoutes from "./modules/auth/auth.routes.js";
 
 const app = express();
 
-/**
- * ✅ CORS CONFIG
- * Frontend: http://localhost:5173
- */
+
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -22,11 +20,11 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 // Health check
-app.get("/", (_req, res) => {
-  res.json({
-    success: true,
-    message: "🚀 BharatGram Backend API running",
-  });
-});
+// app.get("/", (_req, res) => {
+//   res.json({
+//     success: true,
+//     message: "🚀 BharatGram Backend API running",
+//   });
+// });
 
 export default app;
